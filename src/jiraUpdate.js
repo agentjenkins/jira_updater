e70528f5240b9b27f4eed58050e4c42a4743dd63
@blomar <martin.blomsterext@delaval.com>
2017-12-04 13:25:36 +0100

    refactored update to have its own file

--- src/jiraUpdate.js
+++ src/jiraUpdate.js
@@ -2,6 +2,7 @@ JiraApi = require('jira').JiraApi
 var props = require('./resources/props.json')
 var argv = require('minimist')(process.argv.slice(2))
 var list = require('./list.js');
+var update = require('./update.js');
 var jira = new JiraApi(props.protocol, props.url, props.port, props.user, props.password, props.apiVersion, props.verbose, props.strictSsl)
 
 console.dir(argv)
@@ -11,6 +12,9 @@ var issues = require(argv.issues)
 
 if (argv.list) {
     list(jira, issues);
-  } else {
+  } else if (argv.update) {
+    update(jira, issues);
+  }
+  else {
     console.log("Unknown command, valid commands are --list --update");
   }
--- src/update.js
+++ src/update.js
@@ -1,26 +1,25 @@
-JiraApi = require('jira').JiraApi;
-var props = require('./resources/props.json');
+'use strict'
 
-var jira = new JiraApi(props.protocol, props.url, props.port, props.user, props.password, props.apiVersion, props.verbose, props.strictSsl)
-
-var issues = require('./resources/reportItems1.json')
-
-for (i = 0; i < issues.length; i++) {
-console.log(issues[i].key);
-jira.updateIssue(issues[i].key, {
-    "update": {
-        "timetracking": [
-            {
-                "edit": {
-                    "originalEstimate": "8h"
-                }
+var update = function (jira, issues) {
+  for (var i = 0; i < issues.length; i++) {
+    console.log(issues[i].key)
+    jira.updateIssue(issues[i].key, {
+      'update': {
+        'timetracking': [
+          {
+            'edit': {
+              'originalEstimate': '8h'
             }
+          }
         ]
-    }
-}, function(error, issue) {
-    console.log(issue);
-    if(error) {
-      console.log("error: " + error);
-    }
-  });
-}
+      }
+    }, function (error, issue) {
+      console.log(issue)
+      if (error) {
+        console.log('error: ' + error)
+      }
+    })
+  }
+};
+
+module.exports = update;
