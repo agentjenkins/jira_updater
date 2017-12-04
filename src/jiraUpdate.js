7136e6b1d5d775777ddd271dc7ab4c989e9d5685
BlomsterEXT, Martin <martin.blomsterext@delaval.com>
2017-12-04 16:27:15 +0100

    Formatted

--- src/jiraUpdate.js
+++ src/jiraUpdate.js
@@ -1,18 +1,17 @@
 JiraApi = require('jira').JiraApi
 var props = require('../resources/props.json')
 var argv = require('minimist')(process.argv.slice(2))
-var list = require('./ops/list.js');
-var update = require('./ops/update.js');
+var list = require('./ops/list.js')
+var update = require('./ops/update.js')
 var jira = new JiraApi(props.protocol, props.url, props.port, props.user, props.password, props.apiVersion, props.verbose, props.strictSsl)
 
 // Require file containing issue keys
 var issues = require(argv.issues)
 
 if (argv.list) {
-    list(jira, issues);
-  } else if (argv.update) {
-    update(jira, issues);
-  }
-  else {
-    console.log("Unknown command, valid commands are --list --update");
-  }
+  list(jira, issues)
+} else if (argv.update) {
+  update(jira, issues)
+} else {
+  console.log('Unknown command, valid commands are --list --update')
+}
