61af442a5feaef7e890984820e64778db408df19
BlomsterEXT, Martin <martin.blomsterext@delaval.com>
2017-12-04 16:26:14 +0100

    Moved operations to subfolder

--- src/jiraUpdate.js
+++ src/jiraUpdate.js
@@ -1,11 +1,9 @@
 JiraApi = require('jira').JiraApi
 var props = require('../resources/props.json')
 var argv = require('minimist')(process.argv.slice(2))
-var list = require('./list.js');
-var update = require('./update.js');
+var list = require('./ops/list.js');
+var update = require('./ops/update.js');
 var jira = new JiraApi(props.protocol, props.url, props.port, props.user, props.password, props.apiVersion, props.verbose, props.strictSsl)
-
-console.dir(argv)
 
 // Require file containing issue keys
 var issues = require(argv.issues)
