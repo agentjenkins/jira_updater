02f8731068f7fff0c744db751d8e130d6b58d528
BlomsterEXT, Martin <martin.blomsterext@delaval.com>
2018-01-22 13:03:50 +0100

    Regexp filter on versions

--- src/ops/listVersions.js
+++ src/ops/listVersions.js
@@ -1,24 +1,27 @@
 'use strict'
 
-var listVersions = function(jira, projectKey) {
+var listVersions = function(jira, projectKey, versionRegExp) {
 
   var project = jira.project.getProject({
     'projectIdOrKey': projectKey
   }, function(error, project) {
     console.log(projectKey);
     if (!error) {
-      console.log(project)
+      //console.log(project)
       for (var i = 0; i < project.versions.length; i++) {
         var version = project.versions[i];
-        if (version.name.startsWith('DDM')) {
+        var regExp = new RegExp(versionRegExp);
+        if (regExp.test(version.name)) {
           console.log(version.name + ", " + (version.released ? version.releaseDate : "unreleased"));
+        } else {
+          console.log(miss);
         }
+
       }
     } else {
       console.log(error)
     }
   })
 };
-
 
 module.exports = listVersions;
