61aa16e74b21cc74f042279edd6afd151c5a48d7
BlomsterEXT, Martin <martin.blomsterext@delaval.com>
2018-01-22 12:40:12 +0100

    listVersions implemented

--- src/jiraUpdate.js
+++ src/jiraUpdate.js
@@ -7,30 +7,41 @@ var props = require('../resources/props.json')
 var argv = require('minimist')(process.argv.slice(2))
 var list = require('./ops/list.js')
 var update = require('./ops/update.js')
+var listVersions = require('./ops/listVersions.js')
 var fileReader = require('./util/fileReader')
 var JiraClient = require('jira-connector');
 
-  var jira = new JiraClient( {
-      host: props.url,
-      protocol: props.protocol,
-      port: props.port,
-      basic_auth: {
-          username: props.user,
-          password: props.password
-      },
-      version: props.version
-  });
+var jira = new JiraClient({
+  host: props.url,
+  protocol: props.protocol,
+  port: props.port,
+  basic_auth: {
+    username: props.user,
+    password: props.password
+  },
+  version: props.version
+});
 
 // Require file containing issue keys
-var issues = require(argv.issues);
+function getIssues(argv) {
+  if (!argv.issues) {
+    console.log("--issues are required");
+    throw exception;
+  } else {
+    return require(argv.issues);
+  }
+}
 
 if (argv.list) {
+  var issues = getIssues(argv)
   list(jira, issues, argv)
 } else if (argv.update) {
-
-  var updateJson = ""//fileReader.read(argv.updateJson);
+  var updateJson = "" //fileReader.read(argv.updateJson);
+  var issues = getIssues(argv)
   console.log(updateJson);
   update(jira, issues, updateJson);
-} else {
+} else if (argv.listVersions)
+  listVersions(jira, argv.projectKey);
+else {
   console.log('Unknown command, valid commands are --list --update')
 }
--- src/ops/update.js
+++ src/ops/update.js
@@ -3,23 +3,21 @@
 var update = function(jira, issues) {
   for (var i = 0; i < issues.length; i++) {
     console.log(issues[i].key)
-    debugger;
-    jira.updateIssue(issues[i].key, {
-          'update': {
-            "fields": {
-              "assignee": {
-                "id": "blomma"
-              }
-            }
+    jira.issue.editIssue({'issueKey': issues[i].key, 'issue':  {
+      'update': {
+        'timetracking': [{
+          'edit': {
+            'originalEstimate': '8h'
           }
-        },
-      function(error, issue) {
-        console.log('trying to update ' + issue)
-        if (error) {
-          console.log('error in update: ' + error)
-        }
-      })
-}
+        }]
+      }}
+    }, function(error, issue) {
+      console.log('trying to update ' + issue)
+      if (error) {
+        console.log('error in update: ' + error)
+      }
+    })
+  }
 };
 
 module.exports = update;
